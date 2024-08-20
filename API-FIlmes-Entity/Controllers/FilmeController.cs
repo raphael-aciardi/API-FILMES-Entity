39612a3deea5088e94c1025e71a61431bc7d989a
raphael-aciardi <996193248ph@gmail.com>
2024-08-20 10:38:20 -0300

    Inserindo a classe filme na pasta models e configurando o controlador

---
+++ API-FIlmes-Entity/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using API_FIlmes_Entity.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_FIlmes_Entity.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    private static List<Filme> filmes = new List<Filme>();
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+}
---
+++ API-FIlmes-Entity/Models/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_FIlmes_Entity.Models;
+
+public class Filme
+{
+
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    public String Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    public String Genero {  get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    public int Duracao { get; set; }
+}
