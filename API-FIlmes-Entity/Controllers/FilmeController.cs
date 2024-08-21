d9cfa415efdfa422c82803d8f99f39ab95581d4d
raphael-aciardi <996193248ph@gmail.com>
2024-08-21 10:51:05 -0300

    Adicionando delete, Dtos de leitura, configurando documentação e documentando o método de leitura

--- API-FIlmes-Entity/Controllers/FilmeController.cs
+++ API-FIlmes-Entity/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using API_FIlmes_Entity.Data.Dtos;
 using API_FIlmes_Entity.Models;
 using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_FIlmes_Entity.Controllers;
@@ -19,7 +20,14 @@ public class FilmeController : ControllerBase
         _mapper = mapper;
     }
 
+    /// <summary>
+    /// Adiciona um filme ao banco de dados
+    /// </summary>
+    /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso inserção seja feita com sucesso</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult AdicionarFilme([FromBody] CreateFilmeDto filmeDto)
     {
         Filme filme = _mapper.Map<Filme>(filmeDto);
@@ -31,9 +39,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return _context.Filmes.Skip(skip).Take(take);
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
@@ -45,7 +53,8 @@ public class FilmeController : ControllerBase
         {
             return NotFound();
         }
-        return Ok(filme);
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+        return Ok(filmeDto);
     }
 
     [HttpPut("{id}")]
@@ -54,6 +63,35 @@ public class FilmeController : ControllerBase
         var filme = _context.Filmes.FirstOrDefault(
             filme => filme.Id == id); if (filme == null) return NotFound();
             _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult AtualiaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch )
+    {
+        var filme = _context.Filmes.FirstOrDefault(
+            filme => filme.Id == id); 
+        if (filme == null) return NotFound();
+
+        var filmeParaAtualizar = _mapper.Map <UpdateFilmeDto>(filme);
+
+        patch.ApplyTo(filmeParaAtualizar, ModelState);
+
+        if (!TryValidateModel(filmeParaAtualizar)){
+            return ValidationProblem();
+        }
+        _mapper.Map(filmeParaAtualizar, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+      var filme = _context.Filmes.FirstOrDefault(
+     filme => filme.Id == id); if (filme == null) return NotFound();
+        _context.Filmes.Remove(filme);
         _context.SaveChanges();
         return NoContent();
     }
--- API-FIlmes-Entity/obj/Debug/net8.0/API-FIlmes-Entity.AssemblyInfo.cs
+++ API-FIlmes-Entity/obj/Debug/net8.0/API-FIlmes-Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-FIlmes-Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d07c7a826274034bf3bdc4b4d27a68874c5afc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddbcc3afc51067d5715be505fd1718751ad9785")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-FIlmes-Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-FIlmes-Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
